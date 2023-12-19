ec368b42f63c7a4d99d8270761d7199aa572bc43
Artur Moisés Alves de Paula <arturmoiscontato@gmail.com>
2023-12-19 11:43:10 -0300

    feat: add useState hook

--- frontend/src/public/Login/Login.jsx
+++ frontend/src/public/Login/Login.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Login() {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function onChangeEmail(event) {
+    setEmail(event.target.value);
+  }
+
+  function onChangePassword(event) {
+    setPassword(event.target.value);
+  }
+
+  function onSubmit() {
+    console.log(email, password);
+  }
+
   return (
     <div>
       <main>
-        <section className="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
+        <section className="vh-lg-100 mt-10 mt-lg-0 bg-soft d-flex align-items-center">
           <div className="container">
             {/* <p className="text-center">
               <Link to="/" className="d-flex align-items-center justify-content-center">
@@ -24,14 +39,18 @@ export default function Login() {
                 <div className="text-center text-md-center mb-4 mt-md-0">
                   <h1 className="mb-0 h3">Faça login na plataforma</h1>
                 </div>
-                <form >
+                <form action="#" className="mt-4" onSubmit={onSubmit}>
                   <div className="form-group mb-4">
                     {/* <label htmlFor="email">Email</label> */}
                     <div className="input-group">
                       <span className="input-group-text" id="basic-addon1">
-                        <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                        <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z">
+                          </path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z">
+                          </path>
+                        </svg>
                       </span>
-                      <input type="email" className="form-control" placeholder="Email" id="email" autoFocus required />
+                      <input type="email" className="form-control" placeholder="Email" id="email" autoFocus required onChange={onChangeEmail} />
                     </div>
                   </div>
                   <div className="form-group">
@@ -39,9 +58,12 @@ export default function Login() {
                       {/* <label htmlFor="password">Senha</label> */}
                       <div className="input-group">
                         <span className="input-group-text" id="basic-addon2">
-                          <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path></svg>
+                          <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd">
+                            </path>
+                          </svg>
                         </span>
-                        <input type="password" placeholder="Senha" className="form-control" id="password" required />
+                        <input type="password" placeholder="Senha" className="form-control" id="password" required onChange={onChangePassword} />
                       </div>
                     </div>
                     <div className="d-flex justify-content-between align-items-top mb-4">
@@ -51,11 +73,17 @@ export default function Login() {
                           Lembrar-me
                         </label>
                       </div>
-                      <div><Link to="/forgot-password" className="small text-right">Recuperar senha</Link></div>
+                      <div>
+                        <Link to="/forgot-password" className="small text-right">
+                          Recuperar senha
+                        </Link>
+                      </div>
                     </div>
                   </div>
                   <div className="d-grid">
-                    <button type="submit" className="btn btn-gray-800">Entrar</button>
+                    <button type="submit" className="btn btn-gray-800">
+                      Entrar
+                    </button>
                   </div>
                   {/* {
                       error ?
