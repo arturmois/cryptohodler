7cb72b7a29ee8085f186717e30a5993e8f867e1c
Artur Moisés Alves de Paula <arturmoiscontato@gmail.com>
2023-12-19 22:16:18 -0300

    feat(backend): add errorMiddleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import helmet from 'helmet';
+import errorMiddleware from './middlewares/errorMiddleware.js';
 
 const app = express();
 
@@ -11,5 +13,7 @@ app.use(json());
 app.use('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use(errorMiddleware);
 
 export default app;
