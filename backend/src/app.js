9323207f5a462b5ec126db340571618b04e9b489
Artur Moisés Alves de Paula <arturmoiscontato@gmail.com>
2023-12-19 21:58:20 -0300

    feat(backend): add cors helmet and json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(json());
 
 app.use('/', (req, res) => {
   res.send('Hello World');
