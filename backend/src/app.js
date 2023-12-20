2e633e2346c02892f82318e3d879b267b4b6f6ab
Artur Moisés Alves de Paula <arturmoiscontato@gmail.com>
2023-12-19 22:32:32 -0300

    feat(backend): add login and logout routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,8 +10,20 @@ app.use(cors());
 app.use(helmet());
 app.use(json());
 
-app.use('/', (req, res) => {
-  res.send('Hello World');
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email === 'arturmoiscontato@gmail.com'
+    && password === '123456') {
+    res.sendStatus(200);
+  }
+  else
+    res.sendStatus(401);
+});
+
+app.post('/logout', (req, res) => {
+  res.sendStatus(200);
 });
 
 app.use(errorMiddleware);
