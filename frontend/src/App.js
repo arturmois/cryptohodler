ba51672c20a2a8c329d1abcfa59bfa1245fe3464
Artur Moisés <arturmoiscontato@gmail.com>
2023-12-28 17:58:18 -0300

    feat:(front): add private routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import Login from './public/Login/Login';
 import Settings from './private/Settings/Settings';
 
 export default function App() {
+
+  const PrivateRoute = () => {
+    const isAuthenticated = localStorage.getItem('token');
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Login />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route element={<PrivateRoute />} >
+          <Route path="/settings" element={<Settings />} />
+        </Route>
         <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
