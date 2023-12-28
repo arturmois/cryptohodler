9f01c1487ff8b4d63d8c17494be57266625a0012
Artur Moisés <arturmoiscontato@gmail.com>
2023-12-28 15:21:52 -0300

    feat(backend): jwt in the backend

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-unused-vars */
+import jwt from 'jsonwebtoken';
+
 export function doLogin(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
-  console.log(email, password);
+
   if (email === 'arturmoiscontato@gmail.com'
     && password === '123456') {
-    res.sendStatus(200);
+    const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
+      expiresIn: parseInt(process.env.JWT_EXPIRES)
+    });
+    res.json({ token });
   }
   else
     res.sendStatus(401);
